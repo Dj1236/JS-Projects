067bbb5d5d87a80d0df3111e876868bbe348de4e
DHAIRYA_JOSHI <121332557+Dj1236@users.noreply.github.com>
2024-05-30 11:39:48 +0530

    Add files via upload

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,73 @@
+// let js = "JavaScript";
+// if (js === "JavaScript") alert("js is funny");
+// console.log(40 + 50 + 60 + 70 + 80);
+// console.log("dhairyajoshi aa je andar laikhu che e value che ahve aapde e varibale ni andar store kariye");
+// let  name = "Dhairya Joshi"; // pachi aapde aavi rite value assign kari sakiye varible ne 
+// console.log(`My Name Is ${name}`);
+
+// variable always be declare in the camle case notaion
+// never use reserved keyword in the declartion of varaible 
+// every value in the js is object everything else is primitive .
+
+
+// js has a feature of dynamic typing here we do not have to manually define the data type of the value stored in the variable.
+// it dertermind automatically
+
+// const firstName = "Dhairya";
+// const Dob = 2003;
+// const jobDetail = "hi i am student";
+// const currentYear = 2024;
+// const total = "I'm "  + firstName + ", I was born on " + Dob + " and I do " + job + "current year" + currentYear + (Dob -currentYear)+"you are this year old ";
+// let age = currentYear - Dob;
+// console.log(`${firstName} ${age} years old and ${jobDetail}`);  // easy way of doing upar things.
+
+
+// type conversion and type casting(coercion)
+
+// const inputYear = '2003';
+// console.log(Number(inputYear),inputYear);
+// console.log(Number(inputYear + 22));
+
+// truthy and falsy values .
+// 5 falsy values 
+// false, undefined , null , 0 , ""
+
+
+// objects:- 
+// const dhairya  = {
+//     firstName :'dhairya ',
+//     lastName : 'joshi',
+//     age : 18,
+//     job:'web-devloper',
+//     friends:['vatsal','yagnesh','dev','shrey'],
+//     hasLicense: false,
+
+//     birthYear : function(janamSaal){
+//         return 2024- this.age;
+//     }
+
+// };
+
+// loops in array 
+// const dhairyaArray = [
+//     'dhairya',
+//     'joshi',
+//     'web-devloper',
+//     2003-2070,
+//     ['dhairya','joshi','niteshbhai'],
+
+
+// ];
+
+// for (let i = 0; i < dhairyaArray.length; i++) {
+// console.log(dhairyaArray[i],typeof dhairyaArray[i]);
+// }
+
+
+
+
+
+
+
+
+
